0b6a38facbc09513a0bcecfb60e262019f414c87
antoniofdias <antoniofdiass@gmail.com>
2018-12-14 10:05:02 -0300

    Adiciona parâmetros à classe Window, centralizando-a

--- src/domain/window/Window.java
+++ src/domain/window/Window.java
@@ -5,9 +5,14 @@ import java.awt.*;
 
 public class Window extends JFrame {
 	private static final long serialVersionUID = 1L;
-	public Window() {
-		this.setTitle("Janela");
-		setBounds(200,150,400,300);
+	public Window(int windowWidth, int windowHeight, String title) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double screenWidth = screenSize.getWidth();
+		double screenHeight = screenSize.getHeight();
+		this.setTitle(title);
+		int cornerX = (int)screenWidth/2 - windowWidth/2;
+		int cornerY = (int)screenHeight/2 - windowHeight/2;
+		setBounds(cornerX, cornerY, windowWidth, windowHeight);
 		getContentPane(). setBackground( Color .cyan.darker());
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		getRootPane().setBorder(BorderFactory.createLineBorder(Color.red,3));
@@ -15,7 +20,10 @@ public class Window extends JFrame {
 		setUndecorated(false);
 	}
 	public static void main(String args[]) {
-		new	Window().setVisible(true);
+		int width = 800;
+		int height = 600;
+		String title = "Janela";
+		new	Window(width, height, title).setVisible(true);
 	}
 }
 
