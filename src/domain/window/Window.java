42bc6cb09801c197d1637e14830c9d57958f2e46
antoniofdias <antoniofdiass@gmail.com>
2018-12-14 09:33:36 -0300

    Atualiza a classe window usando JFrame

--- src/domain/window/Window.java
+++ src/domain/window/Window.java
@@ -1,5 +1,21 @@
 package domain.window;
 
-public class Window {
+import javax.swing.*;
+import java.awt.*;
 
+public class Window extends JFrame {
+	private static final long serialVersionUID = 1L;
+	public Window() {
+		this.setTitle("Janela");
+		setBounds(200,150,400,300);
+		getContentPane(). setBackground( Color .cyan.darker());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getRootPane().setBorder(BorderFactory.createLineBorder(Color.red,3));
+		//getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+		setUndecorated(false);
+	}
+	public static void main(String args[]) {
+		new	Window().setVisible(true);
+	}
 }
+
